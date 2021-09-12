c9241a993b8f01751c5f09e8e50d699d9f26b0c2
Daniel07C <90563768+Daniel07C@users.noreply.github.com>
2021-09-12 10:43:20 -0400

    Add files via upload

---
+++ app.cpp
@@ -0,0 +1,21 @@
+// This is a basic program on C++
+//
+// Try to modify and run it and check out
+// the output in the terminal below.
+//
+// Happy #include <iostream> 
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int mom;
+    int dad;
+    
+    cout << "How many counter:" << endl;
+    cin >> mom;
+    
+    dad =  mom + 1; 
+    
+    cout << "Dad has " <<  dad << " left. " << endl;
+    return 0;
+}
